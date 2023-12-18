211b7bb49d50f73b117b4cfba2551666e038a4c5
Vincent D <vincent.descales@sfr.fr>
2023-12-18 20:48:56 +0100

    Deploying on vercel
    This is a test for deploying in vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), mdx(), image()],
 	output: 'server',
-  	adapter: netlify(),
+  	adapter: vercel(),
 
 });
