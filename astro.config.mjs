e061f7c0cd5280dcbf3aa733df32fab70539e7aa
Vincent D <vincent.descales@sfr.fr>
2024-01-01 14:48:38 +0100

    Modification

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@ import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), mdx(), image()],
-	output: 'server',
-  	adapter: vercel(),
 
 });
