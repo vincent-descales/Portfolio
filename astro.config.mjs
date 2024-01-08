7056029408731e55dc334ac31d3f1e1034f050e1
Vincent D <vincent.descales@sfr.fr>
2024-01-08 10:44:56 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import netlify from '@astrojs/netlify/functions';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), mdx(), image()],
-	site: 'https://portfolio-vincent-descales.github.io/',
+	site: 'https://vincent-descales.github.io/',
   	base: '/Portfolio',
 });
